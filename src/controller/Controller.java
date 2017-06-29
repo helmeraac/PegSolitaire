eaeec714f478c50318c60b6275ac5e2a83bccfce
Leonardo Avendaño Rocha <leavendanoro@unal.edu.co>
2017-06-29 11:37:13 -0500

    Files Modified

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -13,7 +13,8 @@ import javax.swing.JButton;
 public class Controller implements ActionListener {
     private Game game;
     private ViewInterface view;
-    private int actionCounter;
+    public int oldmovements[];
+    public int newmovements[];
     
     /**
      * Overloaded constructor. Initializes the game and view, and
@@ -25,7 +26,8 @@ public class Controller implements ActionListener {
     public Controller() {
         this.game = new Game();
         this.view = new View();
-        actionCounter=0;
+        newmovements= new int[4];
+        oldmovements= new int[4];
         addActionListeners();
     }
 
@@ -52,21 +54,31 @@ public class Controller implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (!game.isGameOver()) {
-        	actionCounter++;
-        	if(!(actionCounter%2==0)){
-        		game.selectPeg();
-        	}else{
-        		game.movePeg();
-        	}
+        	int indexOfViewButton = getJButtonIndex((JButton) e.getSource());
+            Pair coordinates = convertToCoordinates(indexOfViewButton);
+            for(int i=0;i<4;i++){
+            oldmovements[i]=newmovements[i];
+            }
+            newmovements[0]= indexOfViewButton-14;
+            newmovements[1]= indexOfViewButton+14;
+            newmovements[2]= indexOfViewButton+2;
+            newmovements[3]= indexOfViewButton-2;
+            for(int i=1;i<4;i++){
+            	if(indexOfViewButton==oldmovements[i]){
+            		game.selectPeg(coordinates.first,
+                                   coordinates.second);
+            		view.updateBoardOnMove(newmovements);//TODO
+            	}else{
+            		view.updateBoardOnSelect(oldmovements, newmovements);
+            	}
+            }
+
             game.incTurnCounterAndSetUserSymbol();
 
             // The indices of the View JButton array is 0-8 while the
             // indices of the Game Field array
             // is a 2d 3x3 array, so I have to convert the index
             // into x- and y- coordinates.
-            int indexOfViewButton = getJButtonIndex((JButton) e.getSource());
-
-            Pair coordinates = convertToCoordinates(indexOfViewButton);
             game.setFieldOwner(game.getUserSymbol(),
                                coordinates.first,
                                coordinates.second);
@@ -83,7 +95,7 @@ public class Controller implements ActionListener {
      */
     private int getJButtonIndex(JButton button) {
         int buttonIndex = 0;
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 49; i++) {
             if (button == ((View)view).getButton(i)) {
                 buttonIndex = i;
             }
