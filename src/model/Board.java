be9306af6c9a929e6bf0066dad56838074cb8241
Leonardo Avendaño Rocha <leavendanoro@unal.edu.co>
2017-06-28 20:24:39 -0500

    The code to set up the board with the standard configuration was created.

--- src/model/Board.java
+++ src/model/Board.java
@@ -6,15 +6,23 @@ import model.Field.Symbol;
 public class Board {
 	private Field[][] gameGrid;
     private static final int BOARD_SIDE_LENGTH = 7;
-    //Latin Cross board
+    //Standard configuration board
     public Board() {
         gameGrid = new Field[BOARD_SIDE_LENGTH][BOARD_SIDE_LENGTH];
         
-        for (int j = 1; j <= 4; j++){
-        	gameGrid[3][j] = Field.getDefault();
+        for (int i = 0; i < BOARD_SIDE_LENGTH; i++) {
+            for (int j = 0; j < BOARD_SIDE_LENGTH; j++) {
+                if((i <= 1 && j <=1)||(i <= 1 && j >=5)||(i >= 5 && j <=1)||(i >= 5 && j >=5)||(i == 3 && j == 3)){
+                	gameGrid[i][j] = Field.getNone();
+                }else
+                gameGrid[i][j] = Field.getDefault();
+            }
         }
-        
-        gameGrid[2][2] = Field.getDefault();
-        gameGrid[4][2] = Field.getDefault();
+    }
+    public void setFieldOwner(Symbol owner, int x, int y) {
+        gameGrid[x][y].setOwner(owner);
+    }
+    public Symbol getFieldOwner(int x, int y) {
+        return gameGrid[x][y].getOwner();
     }
 }
